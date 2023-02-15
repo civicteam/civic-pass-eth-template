b350234d4cd0823dcd6506d482cb6af822f4b88b
Kevin Colgan <kevin@civic.com>
2023-02-15 15:48:16 +0100

    CM-1684: ETH RC to 0.7.9-beta.3

--- README.md
+++ README.md
@@ -14,4 +14,7 @@ yarn
 
 # Start the app
 yarn start
+
+# Start the app with the test gatekeeper network
+REACT_APP_GATEKEEPER_NETWORK=tgnuXXNMDLK8dy7Xm1TdeGyc95MDym4bvAQCwcW21Bf yarn start
 ```
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './App.css';
 import logo from './logo.svg';
 import {
@@ -13,8 +13,10 @@ import {
 import { mainnet, goerli, polygon, optimism, arbitrum, polygonMumbai, sepolia, arbitrumGoerli, optimismGoerli } from 'wagmi/chains';
 import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
 import {GatewayProvider, IdentityButton} from "@civic/ethereum-gateway-react";
+import { Signer } from 'ethers';
+import { TypedDataSigner } from '@ethersproject/abstract-signer';
 
-const GATEKEEPER_NETWORK = "ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6";
+const GATEKEEPER_NETWORK = process.env.REACT_APP_GATEKEEPER_NETWORK || "ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6";
 
 const client = createClient(
     getDefaultClient({
@@ -37,15 +39,29 @@ const Content = () => {
 }
 
 const Gateway = () => {
-    const provider = useProvider()
+    const provider = useProvider();
+    const { address, isConnected } = useAccount();
     const { data: signer } = useSigner();
 
-    if (!signer) return <></>
+    const frankenWallet: Signer & TypedDataSigner = useMemo(() => {
+        if (!signer) return null
+        const w = Object.create(signer);
+        w.getAddress = async () => {
+            if (!isConnected) {
+                return undefined;
+            }
+            return address
+        }
+        return w;
+    }, [address, isConnected, signer]);
+
+    if (!frankenWallet) return <></>
 
     return <GatewayProvider
         gatekeeperNetwork={GATEKEEPER_NETWORK}
         provider={provider}
-        signer={signer}
+        signer={frankenWallet}
+        stage={'dev'}
     >
         <Content/>
     </GatewayProvider>
