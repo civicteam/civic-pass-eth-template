48764ba0b447b7302a5dd64e90fc01897a123f5b
Kevin Colgan <kevin@civic.com>
2023-11-07 15:27:12 +0100

    feat: Gateway RC 1.0.0 + connectkit + wagmi + viem

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import logo from './logo.svg';
 import {
-    createClient,
     useAccount,
     useEnsAvatar,
     useEnsName, useNetwork,
-    WagmiConfig
+    WagmiConfig,
+    WalletClient,
+    useWalletClient,
+    configureChains,
+    createConfig,
 } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
 import { mainnet, goerli, polygon, optimism, arbitrum, polygonMumbai, sepolia, arbitrumGoerli, optimismGoerli } from 'wagmi/chains';
-import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
-import {GatewayProvider, IdentityButton} from "@civic/ethereum-gateway-react";
-import {Wallet} from 'ethers';
+import { ConnectKitProvider, ConnectKitButton } from "connectkit";
+import {GatewayProvider, IdentityButton, EthereumGatewayWallet} from "@civic/ethereum-gateway-react";
+import { BrowserProvider, JsonRpcSigner } from 'ethers';
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
 const GATEKEEPER_NETWORK = process.env.REACT_APP_GATEKEEPER_NETWORK || "ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6";
 
@@ -21,18 +26,19 @@ const hash = window.location.hash.replace("#", "");
 const selectedChain = chains[hash as keyof typeof chains];
 const clientChains = selectedChain ? [selectedChain] : Object.values(chains)
 
-const client = createClient(
-    getDefaultClient({
-        appName: 'Civic Pass Eth demo',
-        chains: clientChains,
-    })
-)
 
-const Content = () => {
-    const { address, isConnected } = useAccount()
+type ContentProps = {
+    address?: `0x${string}` | undefined;
+}
+const Content = ({ address }: ContentProps) => {
+    const { isConnected } = useAccount()
     const network = useNetwork()
-    const { data: ensAvatar } = useEnsAvatar({ address })
-    const { data: ensName } = useEnsName({ address })
+    const { data: ensAvatar } = useEnsAvatar({
+        name: address,
+      })
+    const { data: ensName } = useEnsName({
+        address,
+      })
     return <>
         { ensAvatar ?
             <img src={ensAvatar} className="App-logo" alt="logo" /> :
@@ -42,15 +48,34 @@ const Content = () => {
         {isConnected && <IdentityButton/>}
     </>
 }
+const walletClientToSigner = (walletClient: WalletClient) => {
+    const { account, chain, transport } = walletClient;
+    const network = {
+      chainId: chain?.id,
+      name: chain?.name,
+      ensAddress: chain?.contracts?.ensRegistry?.address,
+    };
+  
+    const provider = new BrowserProvider(transport, network);
+    const signer = new JsonRpcSigner(provider, account?.address);
+    return signer;
+  }
 
-const useWallet = ():Wallet | undefined => {
-    const { connector, address } = useAccount();
-    const [wallet, setWallet] = useState<Wallet>();
-    // update the wallet if the connector or address changes
+const useWallet = (): EthereumGatewayWallet | undefined => {
+    const { data: walletClient } = useWalletClient();
+    const [wallet, setWallet] = useState<EthereumGatewayWallet>();
+
     useEffect(() => {
-        if (!connector) return;
-        connector.getSigner().then(setWallet);
-    }, [connector, address]);
+        // the wallet client chain is set asynchronously, so wait until
+        // it's set before setting the wallet
+        if (!walletClient?.chain) {
+          return setWallet(undefined);
+        }
+        if (walletClient && walletClient?.account) {
+          const signer = walletClientToSigner(walletClient);
+          setWallet({ address: walletClient.account?.address, signer });
+        }
+      }, [walletClient]);
 
     return wallet;
 }
@@ -63,14 +88,25 @@ const Gateway = () => {
         gatekeeperNetwork={GATEKEEPER_NETWORK}
         wallet={wallet}
     >
-        <Content/>
+        <Content address={wallet.address as `0x${string}`} />
     </GatewayProvider>
 }
+const { publicClient, webSocketPublicClient } = configureChains(
+    Object.values(chains), // Things start to break if you only have one chain here.
+    [publicProvider()]
+  );
+const config = createConfig({
+    publicClient,
+    webSocketPublicClient,
+    connectors: [
+      new MetaMaskConnector({ chains: clientChains }),
+    ],
+  });
 
 function App() {
     return (
         <div className="App">
-            <WagmiConfig client={client}>
+            <WagmiConfig config={config}>
                 <ConnectKitProvider theme="auto">
                     <header className="App-header">
                         <ConnectKitButton />
